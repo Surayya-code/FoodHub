8fdb09695ad583d703fcb8dd64dd007cf4b5f9ab
Surayya-code <surayyaah@code.edu.az>
2023-07-14 23:25:12 +0400

    shared preference

---
+++ app/src/main/java/com/example/foodhub/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.foodhub.util
+
+object Constants {
+    const val BASE_URL = "https://api.escuelajs.co/api/v1/"
+    const val SP_BASE="CURRENT_USER"
+    const val TOKEN="TOKEN"
+}
